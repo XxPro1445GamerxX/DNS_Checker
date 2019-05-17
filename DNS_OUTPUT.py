96a702124347a772a6daad73d6db4cab6a518d9c
XxPro1445GamerxX <49099308+XxPro1445GamerxX@users.noreply.github.com>
2019-05-17 16:43:43 -0400

    Update DNS_OUTPUT.py

--- DNS_OUTPUT.py
+++ DNS_OUTPUT.py
@@ -7,6 +7,7 @@ import threading
 from threading import Thread
 from tkinter import *
 print('Finding the IP of google, if successful, you have a connetion to the wifi\nChecking...\n')
+
 def button():
     root = Tk()
         
@@ -21,6 +22,7 @@ def button():
 def entire():
     print('This wil check every 30 seconds\nPress Ctrl C to exit')
     while True:
+
         def check():
             try:
                 addr1 = socket.gethostbyname('google.com')
@@ -37,6 +39,7 @@ def entire():
                 e.setProperty('volume', 0.9)
                 e.say('Failed')
                 e.runAndWait()
+
         def other_check():
 
             if fuct1_thread.isAlive:
@@ -56,6 +59,7 @@ def entire():
             time.sleep(1)
 
 def entire1(event):
+
     def check():
         try:
             addr1 = socket.gethostbyname('google.com')
@@ -72,6 +76,7 @@ def entire1(event):
             e.setProperty('volume', 0.9)
             e.say('Failed')
             e.runAndWait()
+
     def other_check():
         if fuct1_thread.isAlive:
             e = pyttsx3.init()
@@ -83,9 +88,22 @@ def entire1(event):
             
     fuct1_thread = Thread(target = check)
     other_check()
-
+def elses():
+    over = input('Do want to do it automatically(a) or manually(m): ').lower()
+    score = 0
+    if over == 'm' or over == 'a':
+        score = 1
+    while score == 0:
+        print('Thats not one of them! (M or A)')
+        over = input('Do want to do it automatically(a) or manually(m): ').lower()
+        if over == 'm':
+            button()
+        elif over == 'a':
+            entire()
 over = input('Do want to do it automatically(a) or manually(m): ').lower()
 if over == 'm':
     button()
-if over == 'a':
+elif over == 'a':
     entire()
+else:
+    elses()
